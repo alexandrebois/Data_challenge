b919e133addfdeafb1d1a3aa716d92433e2fe780
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2023-11-08 19:22:33 +0100

    V1 YOLO 92,8% sur validation

---
+++ script/utils.py
@@ -0,0 +1,52 @@
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+
+def normalize_images_and_boxes(bbox_data, target_size, image_directory):
+    """
+    Normalize the size of images and bounding boxes.
+
+    Args:
+    bbox_data (pandas.DataFrame): A DataFrame containing information about images and bounding boxes. It should have the following columns:
+        - image_name: Name of the image file.
+        - x1: X-coordinate of the top-left corner of the bounding box.
+        - y1: Y-coordinate of the top-left corner of the bounding box.
+        - x2: X-coordinate of the bottom-right corner of the bounding box.
+        - y2: Y-coordinate of the bottom-right corner of the bounding box.
+    target_size (tuple): A tuple (width, height) specifying the desired size to which images should be resized and normalized.
+    image_directory (str): The directory path where the image files are located.
+
+    Returns:
+    numpy.ndarray: A numpy array containing the preprocessed images.
+
+    numpy.ndarray: A numpy array containing the target labels, i.e., the scaled bounding boxes represented as [x1_scaled, y1_scaled, x2_scaled, y2_scaled].
+    """
+
+    images = []  # List to store the preprocessed images
+    labels = []  # List to store the target labels (scaled bounding boxes)
+
+    for index, row in bbox_data.iterrows():
+        image_name, x1, y1, x2, y2, _, _ = row
+
+        # Load and normalize the image
+        image = load_img(f"{image_directory}/{image_name}", target_size=target_size)
+        image = img_to_array(image) / 255.0
+        images.append(image)
+
+        # Calculate scaling factors for bounding box coordinates
+        original_width, original_height = load_img(
+            f"{image_directory}/{image_name}"
+        ).size
+        scale_x = target_size[0] / original_width
+        scale_y = target_size[1] / original_height
+
+        # Resize and scale bounding box coordinates
+        x1_scaled = x1 * scale_x
+        y1_scaled = y1 * scale_y
+        x2_scaled = x2 * scale_x
+        y2_scaled = y2 * scale_y
+
+        label = [x1_scaled, y1_scaled, x2_scaled, y2_scaled]
+        labels.append(label)
+
+    return np.array(images), np.array(labels)
